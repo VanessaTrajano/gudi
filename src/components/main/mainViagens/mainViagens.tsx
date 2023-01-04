bc15fa1b82aa29272683da414ca98ff2f6e4c012
VanessaTrajano <vanessapalmeirakelmertrajano@gmail.com>
2023-01-04 18:37:07 -0300

    finalização responsivo mobile

--- src/components/main/mainViagens/mainViagens.tsx
+++ src/components/main/mainViagens/mainViagens.tsx
@@ -20,19 +20,23 @@ function MainViagens() {
         <img src={vn1} />
         <ViagensNacionais titulo='O clima perfeito, no lugar perfeito' texto='Planeje sua viagem com a sua paquera, compartilhar momentos preciosos juntos não tem preço.' corFundo1='#005BD5' corFundo2='#002E6B' corGeral='#fff' largura='70%' />
       </div>
-      <div className='boxViagens'>
+      <div className='boxViagensPc'>
         <ViagensNacionais titulo='Curta uma nova vibe entre amigos' texto='Rachando a conta ou rachando o bico, a melhor hora pra curtir é entre amigos. Conheça nossos Planos Multi.' corFundo1='#fff' corFundo2='#fff' corGeral='#005BD5' largura='75%' />
         <img src={vn2} />
+      </div>
+      <div className='boxViagensMobile'>
+        <img src={vn2} />
+        <ViagensNacionais titulo='Curta uma nova vibe entre amigos' texto='Rachando a conta ou rachando o bico, a melhor hora pra curtir é entre amigos. Conheça nossos Planos Multi.' corFundo1='#fff' corFundo2='#fff' corGeral='#005BD5' largura='75%' />
       </div>
       <div className='boxViagens'>
         <img src={vn3} />
         <ViagensNacionais titulo='Algumas experiências são inexplicáveis' texto='Conheça as fontes termais de Caldas Novas, Goiás. Águas quentes, num clima sereno, relaxante e natural.' corFundo1='#005BD5' corFundo2='#002E6B' corGeral='#fff' largura='88%' />
       </div>
       <div className='boxCidades'>
-        <Cidades nome='Rio de Janeiro' image={Rj}/>
-        <Cidades nome='Rio das Ostras' image={RioOstras}/>
-        <Cidades nome='Caldas Novas' image={CaldasNovas}/>
-        <Cidades nome='Amazônia' image={Am}/>
+        <Cidades nome='Rio de Janeiro' image={Rj} />
+        <Cidades nome='Rio das Ostras' image={RioOstras} />
+        <Cidades nome='Caldas Novas' image={CaldasNovas} />
+        <Cidades nome='Amazônia' image={Am} />
       </div>
     </div>
   )
